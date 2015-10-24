bc69bee2301c2e0e57e003cdaddfdcd8a0914314
ajpatel331 <ajpatel331@gmail.cim>
2015-10-24 03:12:00 +0000

    Lesson 6 Assignment FINAL COMMIT

--- Lesson6_Assignment/photos.js
+++ Lesson6_Assignment/photos.js
@@ -1,7 +1,7 @@
 /*    
     Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author: Anushka Patel 
+    Date: October 23rd, 2015
     Filename: photos.js
  */
 
@@ -27,7 +27,35 @@ function rightArrow() {
 
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
-function leftArrow() {
+function leftArrow()
+function previewFive() {
+
+   // create figure and img elements for fifth image
+   var lastFigure = document.createElement("figure");
+
+   lastFigure.id = "fig5";
+   lastFigure.style.zIndex = "5";
+   lastFigure.style.position = "absolute";
+   lastFigure.style.right = "45px";
+   lastFigure.style.top = "67px";
+    
+   var lastImage = document.createElement("img");
+   lastImage.width = "240";
+   lastImage.height = "135";
+  lastFigure.appendChild(lastImage);
+    articleEl.appendChild(lastFigure);
+  //clone figure element for fifth image and edit to be first image
+    var firstFigure = lastFigure.cloneNode(true);
+    firstFigure.id = "fig1";
+    firstFigure.style.right = "";
+    firstFigure.style.left = "45px";
+    articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
+
+}//end of previewFive()
+
+
+}//end of previewFive()
+{
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] - 1) === 0) {
          photoOrder[i] = 5;
@@ -38,6 +66,35 @@ function leftArrow() {
    }
 }
 
+// create event listeners for left arrow, right arrow, and center figure element 
+function createEventListeners() {
+  var leftarrow = document.getElementById("leftarrow");
+  if (leftarrow.addEventListener) {
+     leftarrow.addEventListener("click", leftArrow, false); 
+   } else if (leftarrow.attachEvent)  {
+     leftarrow.attachEvent("onclick", leftArrow);
+   }
+  var rightarrow = document.getElementById("rightarrow");
+    if (rightarrow.addEventListener) {
+        rightarrow.addEventListener("click", rightArrow, false); 
+    } else if (rightarrow.attachEvent)  {
+        rightarrow.attachEvent("onclick", rightArrow);
+    }
+  var mainFig = document.getElementsByTagName("img")[1];
+   if (mainFig.addEventListener) {
+     mainFig.addEventListener("click", zoomFig, false); 
+   } else if (mainFig.attachEvent)  {
+     mainFig.attachEvent("onclick", zoomFig);
+   }
+  var showAllButton = document.querySelector("#fiveButton p");
+    if (showAllButton.addEventListener) {
+        showAllButton.addEventListener("click", previewFive, false);
+    } else if (showAllButton.attachEvent) {
+        showAllButton.attachEvent("onclick", previewFive);
+    }
+}
+
+}//end of createEventListerners function
 
 
 /* open center figure in separate window */
@@ -51,6 +108,16 @@ function zoomFig() {
 function setUpPage() {
    createEventListeners();
    populateFigures();
+  function populateFigures() {
+    var filename;
+    var currentFig;
+
+        for (var i = 1; i < 4; i++) {
+            filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+            currentFig = document.getElementsByTagName("img")[i - 1];
+            currentFig.src = filename;
+        }//end of loop
+
 }
 
 
